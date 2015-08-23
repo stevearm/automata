c851482a90f57b296a3db016262b91820941b746
Steve Armstrong <steve@horsefire.com>
2015-08-22 23:12:28 -0700

    Switching diffusion test to be omni-directional

--- src/automata.py
+++ src/automata.py
@@ -29,16 +29,21 @@ def distribute_sim():
     def get_field_as_rgb():
         return render.scale_and_convert_to_rgb(field, 0, people_per_spot_range[1] + 1)
 
-    render.save_rgb_grid_as_image(get_field_as_rgb(), filename="output.start.png")
+    # render.save_rgb_grid_as_image(get_field_as_rgb(), filename="output.start.png")
 
     def make_frame(t):
         move_people(field, people)
         return get_field_as_rgb()
 
-    render.make_video(make_frame, mp4_filename="output.mp4", seconds=5, fps=10)
+    # render.make_video(make_frame, mp4_filename="output.mp4", seconds=5, fps=10)
     # render.make_video(make_frame, gif_filename="output.gif", seconds=5, fps=4)
 
-    render.save_rgb_grid_as_image(get_field_as_rgb(), filename="output.end.png")
+    # render.save_rgb_grid_as_image(get_field_as_rgb(), filename="output.end.png")
+
+    for i in range(20):
+        for times in range(3):
+            move_people(field, people)
+        render.save_rgb_grid_as_image(get_field_as_rgb(), filename="output.{0:03}.png".format(i))
 
 def add_people(field, people_list, people_count_range):
     x_max = len(field)
@@ -63,15 +68,17 @@ def move_person(field, person):
     x_max = len(field)
     y_max = len(field[0])
     current_density = field[my_x, my_y]
+    new_x = my_x
+    new_y = my_y
     for x in range(max(0,my_x-1), min(x_max,my_x+2)):
         for y in range(max(0,my_y-1), min(y_max,my_y+2)):
-            if field[x,y] < current_density:
-                # print "Moving from {} to {}".format(person, dict(x=x,y=y))
-                field[my_x, my_y] -= 1
-                field[x, y] += 1
-                person["x"] = x
-                person["y"] = y
-                return
+            if field[x,y] < field[new_x, new_y]:
+                new_x = x
+                new_y = y
+    field[my_x, my_y] -= 1
+    field[new_x, new_y] += 1
+    person["x"] = new_x
+    person["y"] = new_y
 
 if __name__ == "__main__":
     main()
